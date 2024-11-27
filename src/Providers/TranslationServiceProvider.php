0276eae060d85009b0accfdff3ae9870addc7985
Dev.Alysonh <dev.alysonh@gmail.com>
2024-11-26 23:28:36 -0300

    created the translations service provider

---
+++ src/Providers/TranslationServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DevAlysonh\InertiaVueTranslate\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranslationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../src/Middleware/ShareTranslations.php' => app_path('Http/Middleware/ShareTranslations.php'),
+        ], 'middleware');
+        
+        $this->publishes([
+            __DIR__.'/../resources/js/mixins/translations.js' => resource_path('js/mixins/translations.js'),
+            __DIR__.'/../resources/lang' => resource_path('lang'),
+        ], 'translations');
+    }
+
+    public function register()
+    {
+        //
+    }
+}
